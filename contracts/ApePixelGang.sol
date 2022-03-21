3e98a1fd714e9bd2240e719de4e0bcaff804c1d0
PierreLvl <pierre.louvel@protonmail.com>
2022-03-21 12:12:19 +0100

    add support for apecoin payment

--- contracts/ApePixelGang.sol
+++ contracts/ApePixelGang.sol
@@ -7,16 +7,18 @@
 pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
-import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "openzeppelin-solidity/contracts/token/ERC20/utils/SafeERC20.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
 contract ApePixelGang is ERC721, Ownable {
+    using SafeERC20 for IERC20;
     using Strings for uint256;
 
     uint256 public tokenCounter;
     uint256 public constant MAX_QTY = 10000;
     uint256 public price = 0.05 ether;
+    uint256 public apecoinPrice = 15e18;
     uint256 public startBlock;
 
     address public constant boredApeContract =
@@ -25,19 +27,19 @@ contract ApePixelGang is ERC721, Ownable {
         0x2E2435DBADf4e825Ef7C83d871C0aB272EEcD438;
     address public constant founder2 =
         0xf1088C8a435e46f8e75EAcd62Df584D96B310866;
+    address public constant apecoin =
+        0xf1088C8a435e46f8e75EAcd62Df584D96B310866;
 
     string baseTokenURI;
     string public notRevealedURI;
     bool public revealed = false;
-
-    event CollectibleCreated(uint256 indexed id);
 
     constructor(string memory _initNotRevealedURI)
         ERC721("Ape Pixel Gang", "APG")
     {
         tokenCounter = 0;
         setNotRevealedURI(_initNotRevealedURI);
-        
+
         // Mint 30 Ape Pixel Gang for the team & giveaway
         for (uint256 i = 0; i < 30; i++) {
             tokenCounter = tokenCounter + 1;
@@ -74,7 +76,29 @@ contract ApePixelGang is ERC721, Ownable {
         for (uint256 i = 0; i < _num; i++) {
             tokenCounter = tokenCounter + 1;
             _safeMint(msg.sender, tokenCounter);
-            emit CollectibleCreated(tokenCounter);
+        }
+    }
+
+    // Mint Function - accept APECOIN as payment
+    function mint(uint256 _num) public {
+        require(
+            mintingStarted(),
+            "It is not time to mint the collectible yet !"
+        );
+        require(tokenCounter < MAX_QTY, "All collectible have been minted !");
+        require(
+            _num > 0 && _num <= 10,
+            "You can mint no fewer than 1, and no more than 10 collectible at a time"
+        );
+        require(
+            tokenCounter + _num <= MAX_QTY,
+            "I'm afraid you've failed to mint too many collectible"
+        );
+
+        for (uint256 i = 0; i < _num; i++) {
+            tokenCounter = tokenCounter + 1;
+            IERC20(apecoin).safeTransferFrom(msg.sender, address(this), apecoinPrice);
+            _safeMint(msg.sender, tokenCounter);
         }
     }
 
@@ -145,19 +169,20 @@ contract ApePixelGang is ERC721, Ownable {
         _widthdraw(founder1, address(this).balance / 2);
         // 40% to Founder 2 address
         _widthdraw(founder2, address(this).balance);
+        forwardERC20(apecoin);
     }
 
     // ERC20 recovery function
     // 20% is sent to the Bored Ape Treasury, remaining 80% split among the founders.
     function forwardERC20(address _token) public onlyOwner {
         uint256 balance = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(boredApeContract, (balance * 20) / 100);
+        IERC20(_token).safeTransfer(boredApeContract, (balance * 20) / 100);
 
         balance = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(founder1, balance / 2);
+        IERC20(_token).safeTransfer(founder1, balance / 2);
 
         balance = IERC20(_token).balanceOf(address(this));
-        IERC20(_token).transfer(founder2, balance);
+        IERC20(_token).safeTransfer(founder2, balance);
     }
 
     // Set the revealed boolean to true
